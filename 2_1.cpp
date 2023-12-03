5f4c4fc3bd0aa72e64b6dab53ab7dfb6d430928a
DaddyWarcrimes <4444646+DaddyWarcrimes@users.noreply.github.com>
2023-12-03 12:40:09 -0600

    Day 2, puzzle 1 complete

--- 2_1.cpp
+++ 2_1.cpp
@@ -4,65 +4,75 @@ using namespace std;
 
 int main(){
 
-	ifstream infile;
 	string line;
-	int cubeMin[3] = {12,13,14};
-	int red = 12;
-	int green = 13;
-	int blue =14;
+	int cubeMin[3] = {12,13,14}; //red, green and blue maximum possible values
+	string color[3] = {"red","green","blue"}; //So we can for loop through the colors
+	int sum = 0; //Score
+
+	ifstream infile;
 	infile.open("2_1.txt");
-	int sum = 0;
 	if(!infile){
 		cout << "Failed to open\n";
 		return 1;
 	}
 	
 	while(!infile.eof()){
+		//Read line by line
 		getline(infile,line);
 		if(0 == line.length()){
 			continue;
 		}
+//		int sets = 1;
+		int end = line.find(";"); //location of the first set delimiter
+/*		while (end > 0){
+			end = line.find(";",end + 1);
+			sets++;
+		}*/
+
 		int x = line.find(":");
-		int game = stoi(line.substr(5,x - 5));
-		int sub = 0;
-		int set = x;
-		int redS;
-		int greenS;
-		int blueS;
-		string color[3] = {"red","green","blue"};
-		for(int i = 0; i < 3; i++){
-			int beg = line.find(color[i]);
+		int game = stoi(line.substr(5,x - 5)); // Integer value of the game number
+	
+		bool met = true; //Assume the game is possible
+		int beg = x; //Beginning of the first set
+//		end = line.find(";");
+		bool exit = false;
 
-
-		}
-		
-		int r = line.find("red");
-		int rb = line.find(" ",r - 5);
-		int re = line.find(" ", rb + 1);
-		int len = re - rb - 1;
-		
-
-
-
-		int g = line.find("green");
-		int b = line.find("blue");
-
-
-
-		while (set > 0){
-			int f = line.find(";",set);
-			if (f > 0){
-				sub++;
+		while(!exit){
+			if (0 == beg){
+				exit = true;
+				continue;
 			}
-			set = f + 1;
-			int g = line.find(";",set);
-
-
+			//Loop through colors
+			for(int i = 0; i < 3; i++){
+				if(!met){
+					continue;
+				}
+				//Find the integer value for each color in the set
+				int nb = line.find(" ",line.find(color[i],beg) - 5);
+				if (0 > nb || (end < nb && end != -1)){
+					continue;
+				}
+				int ne = line.find(" ",nb + 1);
+				int val = stoi(line.substr(nb + 1, ne - nb - 1));
+				//Check the color value against the maximum possible cubes
+				if(val > cubeMin[i]){
+					met = false;
+					exit = true;
+					cout << game << " is impossible :" << color[i] << "-" << val << endl;;
+					continue;
+				}
+			}
+			//Move to next set
+			beg = end + 1;
+			end = line.find(";",beg);
 		}
-
-		cout << game << " - " << sub << endl;
-
+		//Tally the score
+		if (met){
+			sum += game;
+		}
 	}
+	//Print the final score
+	cout << "\n\nFinal Sum: " << sum << endl;
 	return 0;
 }
 
